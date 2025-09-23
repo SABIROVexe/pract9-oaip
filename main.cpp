e30dd6326c1da62f90b1e5ae8e89e8ea6c2237cf
SABIROVexe <sabirov.main@gmail.com>
2025-09-23 10:01:22 +0300

    реализовал шапку программы

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <clocale>
+
 using namespace std;
 
+void head(){
+    cout << "+------------------------+" << endl;
+    cout << "|Практическая работа #9  |" << endl;
+    cout << "+------------------------+" << endl;
+    cout << "|Выполнил: Сабиров Ильназ|" << endl;
+    cout << "|Группа: П-23            |" << endl;
+    cout << "+------------------------+" << endl;
+}
+
 int main(){
-    cout << "hello \n";
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    
+    head();
+    
     return 0;
 }
